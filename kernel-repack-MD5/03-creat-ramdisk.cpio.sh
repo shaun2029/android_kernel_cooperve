2c2ac522089d348f132ebd6aa26815c424e43371
RafaelBaugis <rafael.baugis.rab@gmail.com>
2012-07-07 17:51:16 -0300

    update

--- kernel-repack-MD5/03-creat-ramdisk.cpio.sh
+++ kernel-repack-MD5/03-creat-ramdisk.cpio.sh
@@ -3,5 +3,6 @@
 if [ -d ramdisk ]; then
 	cd ./ramdisk/ && rm ../boot.img-ramdisk.cpio; clear; sleep 5; find . | cpio -o -H newc > ../boot.img-ramdisk.cpio
 else
-	rm boot.img-ramdisk.cpio && ./02-decompress-ramdisk.sh
+	rm boot.img-ramdisk.cpio
+	./02-decompress-ramdisk.sh
 fi
