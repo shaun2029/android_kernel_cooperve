1e71295b9aa675140fa8c4da741acbdeab235403
RafaelBaugis <rafael.baugis.rab@gmail.com>
2012-07-14 10:10:08 -0300

    swap compiler

--- clean_all.sh
+++ clean_all.sh
@@ -4,8 +4,8 @@ rm -Rf ./logs/ ./kernel-repack-MD5/boot.im* ./kernel-repack-MD5/zImage ./kernel-
 rm -Rf ./kernel-repack-MD5/CWM_kernel/boot.img ./kernel-repack-MD5/CWM_kernel/system/lib/modules/*.ko; clear
 
 export ARCH=arm
-export CROSS_COMPILE=arm-eabi-
-export PATH=~/toolchain/arm-eabi-4.4.3/bin:$PATH
+export CROSS_COMPILE=arm-bcm21553-eabi-
+export PATH=~/toolchain/arm-bcm21553-eabi/bin:$PATH
 
 cd common
 
