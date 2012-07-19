7b877b0cdbb19bbde0838165654fbe1025f72bee
rafael <rafael@baugis.(none)>
2012-07-19 17:47:58 -0300

    update

--- kernel-repack-MD5/03-compress-ramdisk.gz.sh
+++ kernel-repack-MD5/03-compress-ramdisk.gz.sh
@@ -3,4 +3,5 @@ if [ -d ramdisk ]; then
 	cd ./ramdisk/ && rm ../boot.img-ramdisk.cpio.gz; clear; sleep 5; find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.cpio.gz
 else
 	./02-decompress-ramdisk.sh && cd ./ramdisk/ && rm ../boot.img-ramdisk.cpio.gz; clear; sleep 5; find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.cpio.gz
+	cd ..; rm -Rf ramdisk
 fi
--- kernel-repack-MD5/03-compress-ramdisk.lzma.sh
+++ kernel-repack-MD5/03-compress-ramdisk.lzma.sh
@@ -3,4 +3,5 @@ if [ -d ramdisk ]; then
 	cd ./ramdisk/ && rm ../boot.img-ramdisk.cpio.lzma; clear; sleep 5; find . | cpio -o -H newc | lzma > ../boot.img-ramdisk.cpio.lzma
 else
 	./02-decompress-ramdisk.sh && cd ./ramdisk/ && rm ../boot.img-ramdisk.cpio.lzma; clear; sleep 5; find . | cpio -o -H newc | lzma > ../boot.img-ramdisk.cpio.lzma
+	cd ..; rm -Rf ramdisk
 fi
