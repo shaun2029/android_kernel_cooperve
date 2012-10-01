1066e2e1a42929ae8ac8b3cd2556c72d01cc4c60
root <root@baugis.(none)>
2012-10-01 20:21:16 -0300

    update

---
+++ kernel-repack-MD5/ramdisk/sbin/root_install.sh
@@ -0,0 +1,23 @@
+#!/sbin/sh
+
+if /sbin/test ! -f /system/xbin/su; then
+	/sbin/cp /sbin/su-bin /system/xbin/su
+	/sbin/chmod 6755 /system/xbin/su
+else
+	/sbin/chmod 6755 /system/xbin/su
+fi
+
+if -f /system/sbin/su; then
+	/sbin/rm -Rf /system/sbin/su
+fi
+
+if -f /system/bin/su; then
+	/sbin/rm -Rf /system/bin/su
+fi
+
+if /sbin/test ! -f /system/Superuser.apk || -f /system/Supersu.apk; then
+	/sbin/cp /sbin/Superuser.apk /system/app/Superuser.apk
+	/sbin/chmod 0644 /system/Superuser.apk
+else
+	/sbin/chmod 0644 /system/Superuser.apk
+fi
