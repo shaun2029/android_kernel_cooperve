cc5b5f662e532e95e13205c885fbb75917c47062
root <root@baugis.(none)>
2012-10-04 12:21:54 -0300

    remove unused files and bad governors, set as default gov lionheart and IO sio.

--- common/drivers/cpufreq/cpufreq_conservative.c
+++ common/drivers/cpufreq/cpufreq_conservative.c
@@ -29,7 +29,7 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(85)
+#define DEF_FREQUENCY_UP_THRESHOLD		(75)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(50)
 
 /*
@@ -46,12 +46,12 @@
 
 static unsigned int min_sampling_rate;
 
-#define LATENCY_MULTIPLIER			(500)
+#define LATENCY_MULTIPLIER			(300)
 #define MIN_LATENCY_MULTIPLIER			(100)
-#define DEF_SAMPLING_DOWN_FACTOR		(1)
+#define DEF_SAMPLING_DOWN_FACTOR		(2)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
-#define DEFAULT_FREQUENCY_STEP		(15)
+#define DEFAULT_FREQUENCY_STEP			(15)
 
 static void do_dbs_timer(struct work_struct *work);
 
