01c455914a50b8e366239190180706bcf8f57073
root <root@baugis.(none)>
2012-10-06 13:15:25 -0300

    update

--- git_commit.sh
+++ git_commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 touch clean && ./Compile.sh
 find ./common ./modules -name '.gitignore' -exec rm -Rf -v {} \;
+find ./common ./modules -name '*' -exec chown 1000.1000 -v {} \;
 find ./common ./modules -name '*' -exec chmod 755 -v {} \;
 git add .
 git commit -a && touch commit
